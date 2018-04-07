c25dfdc9a2c85053dfea0cf88977d37aa5868c3e
th0r0nd0r <pjfritch1@gmail.com>
2018-04-07 12:48:34 -0700

    divide

--- quadtree.js
+++ quadtree.js
@@ -8,10 +8,10 @@ class Rectangle {
 
   inBounds(point) {
     return (
-      point.x >= this.x - this.width &&
-      point.x < this.x + this.width &&
-      point.y > this.y - this.height &&
-      point.y <= this.y + this.height
+      point.x >= this.x - this.width / 2 &&
+      point.x < this.x + this.width / 2 &&
+      point.y > this.y - this.height / 2 &&
+      point.y <= this.y + this.height / 2
     );
   }
 }
@@ -28,14 +28,32 @@ class QuadTree {
     this.boundary = boundary;
     this.capacity = 4;
     this.points = [];
+    this.subtrees = {};
   }
 
-  insert() {
-
+  insert(point) {
+    if (this.boundary.inBounds(point) && this.points.length < this.capacity) {
+      this.points.push(point);
+    } else {
+      this.divide();
+    }
   }
 
   divide() {
+    const x = this.boundary.x;
+    const y = this.boundary.y;
+    const w = this.boundary.width;
+    const h = this.boundary.height;
 
+    const nw = new Rectangle(x - w/4, y - h/4, w/2, h/2);
+    const ne = new Rectangle(x + w/4, y - h/4, w/2, h/2);
+    const sw = new Rectangle(x - w/4, y + h/4, w/2, h/2);
+    const se = new Rectangle(x + w/4, y + h/4, w/2, h/2);
+
+    this.subtrees.northWest = new QuadTree(nw);
+    this.subtrees.northEast = new QuadTree(ne);
+    this.subtrees.southWest = new QuadTree(sw);
+    this.subtrees.southEast = new QuadTree(se);
   }
 
 
